d61826ca649f5d5662c0bd7214305f7d1804c93c
Andres01Ricardo <33298566+Andres01Ricardo@users.noreply.github.com>
2023-03-25 13:26:22 -0500

    cambio js

--- javascript/actions.js
+++ javascript/actions.js
@@ -1,5 +1,5 @@
 const toggle = document.querySelector('.toggle');
-const menu = document.querySelector('.menu');
+const menu = document.querySelector('.menuweb');
 
 toggle.addEventListener('click', () => {
   toggle.classList.toggle('active');
@@ -7,26 +7,3 @@ toggle.addEventListener('click', () => {
 });
 
 
-
-const prevButton = document.querySelector('.prev-button');
-const nextButton = document.querySelector('.next-button');
-const images = document.querySelector('.carousel-images');
-
-let currentIndex = 0;
-const totalImages = images.children.length;
-
-function showImage() {
-  images.style.marginLeft = `-${currentIndex * 100}%`;
-}
-
-prevButton.addEventListener('click', () => {
-  currentIndex = (currentIndex === 0) ? totalImages - 1 : currentIndex - 1;
-  showImage();
-});
-
-nextButton.addEventListener('click', () => {
-  currentIndex = (currentIndex === totalImages - 1) ? 0 : currentIndex + 1;
-  showImage();
-});
-
-showImage();
